ddacbfad58caa6f831f8cea7653298912907209f
Brad sawer <100177378@student.burnley.ac.uk>
2022-02-03 15:25:51 +0000

    completed calc2

---
+++ ConsoleAppProject/JAVA/Calc2/src/Calc2/Calc2.java
@@ -0,0 +1,30 @@
+public class Calculator2
+{
+    
+   String Name;
+
+   
+   
+   public int add(int x, int y) {
+       return x + y;
+   }  
+   
+  
+   
+   public int subtract(int x, int y) {
+       return x - y;
+    }  
+
+   public int multiply(int x, int y) {
+       return x * y;
+     }  
+  
+        
+   public int divide(int x, int y) {
+       return x / y;
+    }  
+   
+       public int modlus(int x, int y) {
+       return x + y; // %5 % 4 = 
+    }  
+}
